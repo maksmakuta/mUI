fa81f1d3cd2ae7eb8d79a2b06f06a36451b687b3
lakeFm <makuta.maks.mmc@gmail.com>
2021-10-04 22:35:34 +0300

    [0.0.2a] update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< 3849b5f2a93baeb983cb94d39c55ed655d448e25
-#include <iostream>
-
 int main() {
-    std::cout <<  "terminal version of tetris game (alpha)\n";
-=======
-int main() {
->>>>>>> [0.0.1a] init
-=======
-int main() {
->>>>>>> dev
     return 0;
 }
