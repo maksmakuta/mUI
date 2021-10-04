73b794cc295d23b6b115f7ce08e4e95ef2b2fef8
lakeFm <makuta.maks.mmc@gmail.com>
2021-10-04 20:05:40 +0300

    [0.0.1a] init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
+<<<<<<< 3849b5f2a93baeb983cb94d39c55ed655d448e25
 #include <iostream>
 
 int main() {
     std::cout <<  "terminal version of tetris game (alpha)\n";
+=======
+int main() {
+>>>>>>> [0.0.1a] init
     return 0;
 }
