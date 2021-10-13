9481cdfa23c9897918adec59aafd7bd01914dd78
lakeFm <makuta.maks.mmc@gmail.com>
2021-10-13 16:50:48 +0300

    [1.3.3] ad mouse callbacks

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ class RectView : public View{
 private:
     f32 x,y,w,h;
 public:
-    RectView(f32 _x,f32 _y,f32 _w,f32 _h) : View(){
+    RectView(f32 _x,f32 _y,f32 _w,f32 _h) : View(_x,_y,_w,_h){
         this->x = _x;
         this->y = _y;
         this->w = _w;
@@ -12,16 +12,31 @@ public:
     }
 
     fun onDraw(Canvas *c) override{
-
         c->color("#ffaa66");
         c->drawRect(x,y,w,h);
     }
+
+    fun onHover(Canvas *c) override{
+        this->onDraw(c);
+        c->color("#f00");
+        c->drawText(x + w/2,y +h/2,"HOVER","font",10);
+    }
 };
 
-class MainActivity : public Activity{
+class MainActivity : public Activity,public OnClickListener{
 public:
-    MainActivity() : Activity(){
-        this->setLayout(new RectView(0,0,150,60));
+
+    int s = 0;
+
+    MainActivity() : Activity(),OnClickListener(){
+        auto v = new RectView(0,0,150,60);
+        v->setOnClickListener(this);
+        this->setLayout(v);
+    }
+
+    fun onClick() override{
+        printf("Clicked : %i\n",s);
+        s++;
     }
 };
 
---
+++ src/view/listener/Listener.h
@@ -0,0 +1,11 @@
+#ifndef MUI_LISTENER_H
+#define MUI_LISTENER_H
+
+#include "../../sTypes.h"
+
+class Listener{
+public:
+    Listener() = default;
+};
+
+#endif
