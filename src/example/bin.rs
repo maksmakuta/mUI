d992666ef940f37041c6f229fe0aac51e66f7dc6
maks <makuta.maks.mmc@gmail.com>
2021-12-23 17:40:34 +0200

    [0.6.0.8] add simple window creation with glutin

--- src/example/bin.rs
+++ src/example/bin.rs
@@ -1,3 +1,35 @@
- fn main(){
-    println!("Hello,mUI!");
+
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
+
+fn main() {
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new().with_title("A fantastic window!");
+
+    let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
+
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+
+    println!("Pixel format of the window's GL context: {:?}", windowed_context.get_pixel_format());
+
+    el.run(move |event, _, control_flow| {
+        println!("{:?}", event);
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(physical_size) => windowed_context.resize(physical_size),
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+            
+                windowed_context.swap_buffers().unwrap();
+            }
+            _ => (),
+        }
+    });
 }
