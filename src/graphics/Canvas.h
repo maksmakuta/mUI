e45e4cf3ae4b9d0992b22136f691963010a19a31
maks <makuta.maks.mmc@gmail.com>
2022-01-25 23:20:43 +0200

    [0.6.2.5] remove bgfx

--- src/graphics/Canvas.h
+++ src/graphics/Canvas.h
@@ -2,40 +2,71 @@
 #define CANVAS_H
 
 #include "../types.h"
-#include "Backend.h"
-#include "core/Renderer.h"
 
-class Canvas : public Renderer{
+class Canvas{
 private:
-    Renderer* renderer = null;
-    Backend backend;
+
 public:
-    explicit Canvas(Backend mBackend = Default);
+    explicit Canvas();
     ~Canvas();
 
-    fun beginFrame(f32 w, f32 h) override;
-    fun endFrame() override;
+    fun beginFrame(f32 w, f32 h) {
+
+    }
+    fun endFrame() {
+
+    }
 
     // ============ shape methods ============
-    fun begin() override ;
-    fun moveTo(f32 x,f32 y) override;
-    fun lineTo(f32 x,f32 y) override;
-    fun bezierTo(f32 x,f32 y,f32 x1,f32 y1,f32 x2,f32 y2) override;
-    fun end() override;
+    fun begin() {
 
-    fun rect(f32 x,f32 y,f32 w,f32 h) override;
-    fun circle(f32 x,f32 y,f32 r) override;
-    fun line(f32 x1,f32 y1,f32 x2,f32 y2) override;
-    fun point(f32 x,f32 y) override;
+    }
+    fun moveTo(f32 x,f32 y) {
+
+    }
+    fun lineTo(f32 x,f32 y) {
+
+    }
+    fun bezierTo(f32 x,f32 y,f32 x1,f32 y1,f32 x2,f32 y2) {
+
+    }
+    fun end() {
+
+    }
+
+    fun rect(f32 x,f32 y,f32 w,f32 h) {
+
+    }
+    fun circle(f32 x,f32 y,f32 r) {
+
+    }
+    fun line(f32 x1,f32 y1,f32 x2,f32 y2) {
+
+    }
+    fun point(f32 x,f32 y) {
+
+    }
 
     // ============ matrix methods ============
-    fun translate(f32 x,f32 y) override;
-    fun scale(f32 x,f32 y) override;
-    fun rotate(f32 a) override;
+    fun translate(f32 x,f32 y) {
 
-    fun reset() override;
-    fun push() override;
-    fun pop() override;
+    }
+    fun scale(f32 x,f32 y) {
+
+    }
+    fun rotate(f32 a) {
+
+    }
+
+    fun reset() {
+
+    }
+    fun push() {
+
+    }
+    fun pop(){
+
+    }
 };
 
 #endif
